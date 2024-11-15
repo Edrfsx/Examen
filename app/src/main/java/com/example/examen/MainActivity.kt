095c6701fd1163ff9c553365ad48b4c07181ad0e
Edrfsx <jjjordyorozco@gmail.com>
2024-11-15 16:16:44 +0000

    Segundo commit

--- app/src/main/java/com/example/examen/MainActivity.kt
+++ app/src/main/java/com/example/examen/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,24 +15,49 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.examen.ui.theme.ExamenTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ExamenTheme {
-                Inicio()
-                Conversacion(Mensajes.conversationSample)
+                Scaffold (
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Blue,
+                                titleContentColor = Color.White
+                            ),
+                            title = {
+                                Text(text = "Conversación Noelia")
+                            }
+                        )
+                    }
+                ){ innerPadding ->
+                    Inicio(modifier = Modifier.padding(innerPadding))
+                    Conversacion(Mensajes.conversationSample,
+                        modifier = Modifier.padding(innerPadding))
+                }
+
+
             }
         }
     }
@@ -40,11 +66,22 @@ class MainActivity : ComponentActivity() {
 data class Message(val persona: String, val mensajes: String)
 
 @Composable
-fun Inicio(){
-    Column (modifier = Modifier.fillMaxSize()){
-        Text(text = "Alumno:Jordy")
-        Text(text = "Soy un alumno")
+fun Inicio(modifier: Modifier = Modifier){
+    Row (verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()){
+        Image(painter = painterResource(id = R.drawable.avatar0),
+            contentDescription = null)
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()){
+
+            Text(text = "Alumno:Jordy")
+            Text(text = "Soy un alumno")
+        }
     }
+
 
 
 
@@ -52,6 +89,7 @@ fun Inicio(){
 
 @Composable
 fun MensajeCon(msg : Message){
+
     Row (modifier = Modifier.padding(all = 8.dp)){
         Image(painter = painterResource(id = R.drawable.profesor),
             contentDescription = null,
@@ -82,7 +120,7 @@ fun MensajeCon(msg : Message){
 
 
 @Composable
-fun Conversacion(Mensajes: List<Message>){
+fun Conversacion(Mensajes: List<Message>, modifier: Modifier = Modifier){
     LazyColumn {
         items(Mensajes){mensajes ->
             MensajeCon(mensajes)
